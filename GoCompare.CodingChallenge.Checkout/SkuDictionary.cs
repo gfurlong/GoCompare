3323c53127a0270f146eaa72d985e7ea9c722bc6
gfurlong <geoff_furlong@hotmail.com>
2017-02-09 22:10:30 +0000

    Removed Readme.txt and added missing SkuDictionary.cs class file.

---
+++ GoCompare.CodingChallenge.Checkout/SkuDictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoCompare.CodingChallenge.Checkout
+{
+    /// <summary>
+    /// Alias class for a set of SKU items
+    /// </summary>
+    public class SkuDictionary : Dictionary<char, Sku>
+    {
+    }
+}
